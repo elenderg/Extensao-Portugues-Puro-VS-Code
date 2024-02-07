304c247dcd61561f4647f0d83335858f726e2f1f
Elender <elenderg@gmail.com>
2024-02-07 10:24:19 -0300

    Initial commit

---
+++ syntaxes/teste.js
@@ -0,0 +1,8 @@
+function fazalgo(arg1, param, arg2){
+    x = arg1;
+    console.log('faz algo');
+    return 'faz algo';
+}
+
+//Função para que se faça algo usando um argumento1 e um parâmetro e um argumento 2:
+  //Atribua o argumento1 para um número x
