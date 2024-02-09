66fe294495f1cbbb7c7e87e1eb21900c832894ec
Elender <elenderg@gmail.com>
2024-02-09 19:47:49 -0300

    Ajustado regra

--- syntaxes/teste.js
+++ syntaxes/teste.js
@@ -6,3 +6,5 @@ function fazalgo(arg1, param, arg2){
 
 //Função para que se faça algo usando um argumento1 e um parâmetro e um argumento 2:
   //Atribua o argumento1 para um número x
+
+  fazalgo(teste, true, 3, "teste2");
