fca6bb30f1166826a0f316a2dc685330f36063ff
Elender <elenderg@gmail.com>
2024-02-11 13:28:08 -0300

    Adicionado diversos recursos
    Ajustado:
    parênteses
    Declaração de função
    Invocação de função
    Parâmetros
    Condicionais

--- syntaxes/teste.js
+++ syntaxes/teste.js
@@ -1,8 +1,23 @@
-function fazalgo(arg1, param, arg2){
+function fazalgo(arg1, param, arg2, Person){
     x = arg1;
-    console.log('faz algo');
-    return 'faz algo';
+    if(arg1 == true){
+      console.log('faz algo');
+      return 'faz algo';
+    }
+    while(true){
+      break;
+      
+    }
+    Person.name = "";
+
 }
+
+let Person = {
+  name: "Elender",
+  age: 31
+
+}
+
 
 //Função para que se faça algo usando um argumento1 e um parâmetro e um argumento 2:
   //Atribua o argumento1 para um número x
